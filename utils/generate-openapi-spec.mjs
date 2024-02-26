45ae03f4eaa91b0dec3d1fb76f1df112e30acf7e
Jacob Joseph <jacob@dayflower.io>
2024-02-26 15:24:45 -0600

    added some test code to scrape repo

---
+++ utils/generate-openapi-spec.mjs
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+import axios from 'axios'
+
+import { readFileSync } from 'fs'
+
+import { Command } from 'commander'
+
+const loadJson = filePath => {
+  const data = readFileSync(filePath)
+  return JSON.parse(data.toString())
+}
+
+const packageJSON = loadJson('package.json')
+
+const RepoScraperDocs = async ({ tag, branch }) => {
+  console.log(tag)
+  console.log(branch)
+
+  const createUrl = (tag, branch) => {
+    const urlBase = 'https://api.github.com/repos/metabase/metabase/git/trees/'
+    if (tag !== null) {
+      return `${urlBase}${tag}?recursive=1`
+    }
+
+    return `${urlBase}${branch}?recursive=1`
+  }
+
+  const url = createUrl(tag, branch)
+
+  const data = await axios.get(url)
+
+  const matchingPaths = (data.data.tree).filter(item => {
+    if (item.path.startsWith('docs/api')) {
+      return true
+    }
+
+    return false
+  })
+
+  console.log(matchingPaths)
+
+  // 1. collect relevant paths
+  // 2. retrieve files on relevant paths
+  // 3. parse and interpret files
+}
+
+const cli = new Command()
+
+cli
+  .name('generate-openapi-spec')
+  .description('CLI to generate OpenAPI 3 spec for Metabase API')
+  .version(packageJSON.version)
+
+// cli.command('split')
+//   .description('Split a string into substrings and display as an array')
+//   .argument('<string>', 'string to split')
+//   .option('--first', 'display just the first substring')
+//   .option('-s, --separator <char>', 'separator character', ',')
+//   .action((str, options) => {
+//     const limit = options.first ? 1 : undefined
+//     console.log(str.split(options.separator, limit))
+//   })
+
+cli.command('scrape-repo')
+  .description('Scrape metabase/metabase repo')
+  .option('-t, --tag <tag>', 'tag to scrape', null)
+  .option('-b, --branch <branch>', 'branch to scrape', 'master')
+  .action((options) => {
+    RepoScraperDocs(options)
+  })
+
+cli.command('generate')
+  .description('Generate OpenAPI 3 spec from API docs, code, and enriched docs')
+  .action((options) => {
+    console.log(options)
+  })
+
+cli.parse()
